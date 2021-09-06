8f50ac0b0395dcdd4198f42f469a18a15351c78a
Shephard117 <legovaz91@gmail.com>
2021-09-06 11:26:32 +0700

    Initial Commit

---
+++ SeaFood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeaFood
+//
+//  Created by Дмитрий Скоробогаты on 06.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
